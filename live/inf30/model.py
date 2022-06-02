1569c0648f3bd27878d6b1a63907d9bbafaf360e
NariddhKhean <nariddh97@gmail.com>
2022-06-02 11:16:47 +0200

    Public release commit

---
+++ live/inf30/model.py
@@ -0,0 +1,23 @@
+import time
+from random import random
+
+def call(input_args):
+
+    print(input_args)
+
+    time.sleep(30)  # NOTE: mimicking a long running inference...
+
+    features = [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    16.3738 + random() / 10 - 0.05,
+                    48.2082 + random() / 10 - 0.05,
+                    0.0
+                ]
+            }
+        }
+    ]
+    return features
